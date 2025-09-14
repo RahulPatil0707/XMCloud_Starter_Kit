228b5dc8d64315e28ff26bdbb1553e3068a10fb6
RahulPatil0707 <patil.rahul.singapore@gmail.com>
2025-09-14 19:07:39 +0800

    prettier

--- headapps/nextjs-starter/src/components/HeroBanner.tsx
+++ headapps/nextjs-starter/src/components/HeroBanner.tsx
@@ -13,31 +13,30 @@ type HeroBannerProps = {
 
 const HeroBanner = ({ fields }: HeroBannerProps): JSX.Element => {
   return (
-   <section className="relative flex flex-col md:flex-row items-center justify-between p-10 bg-gray-100">
-  {/* Left side content */}
-  <div className="flex-1 text-left mb-6 md:mb-0 md:pr-10">
-    <h1 className="text-4xl font-bold mb-2">
-      <Text field={fields.Heading} />
-    </h1>
-    <p className="text-lg text-gray-600 mb-4">
-      <Text field={fields.SubHeading} />
-    </p>
-    <div className="text-red-600 font-medium">
-      <Text field={fields.BannerImage} />
-    </div>
-  </div>
+    <section className="relative flex flex-col md:flex-row items-center justify-between p-10 bg-gray-100">
+      {/* Left side content */}
+      <div className="flex-1 text-left mb-6 md:mb-0 md:pr-10">
+        <h1 className="text-4xl font-bold mb-2">
+          <Text field={fields.Heading} />
+        </h1>
+        <p className="text-lg text-gray-600 mb-4">
+          <Text field={fields.SubHeading} />
+        </p>
+        <div className="text-red-600 font-medium">
+          <Text field={fields.BannerImage} />
+        </div>
+      </div>
 
-  {/* Right side image */}
-  <div className="flex-1 flex justify-center">
-    <SitecoreImage
-      field={fields.Image1}
-      className="rounded-xl shadow-md"
-      width={400}
-      height={300}
-    />
-  </div>
-</section>
-
+      {/* Right side image */}
+      <div className="flex-1 flex justify-center">
+        <SitecoreImage
+          field={fields.Image1}
+          className="rounded-xl shadow-md"
+          width={400}
+          height={300}
+        />
+      </div>
+    </section>
   );
 };
 
--- headapps/nextjs-starter/src/lib/next-config/plugins/sass.js
+++ headapps/nextjs-starter/src/lib/next-config/plugins/sass.js
@@ -4,18 +4,18 @@ const SassAlias = require('sass-alias');
 /**
  * @param {import('next').NextConfig} nextConfig
  */
- const sassPlugin = (nextConfig = {}) => {
+const sassPlugin = (nextConfig = {}) => {
   return Object.assign({}, nextConfig, {
-      sassOptions: {
-        importer: new SassAlias({
-          '@sass': path.join(__dirname, '../../../assets', 'sass'),
-          '@fontawesome': path.join(__dirname, '../../../../node_modules', 'font-awesome'),
-        }).getImporter(),
-        // temporary measure until new versions of bootstrap and font-awesome released
-        quietDeps: true,    
-        silenceDeprecations: ["import", "legacy-js-api"],
-      },
-    });
+    sassOptions: {
+      importer: new SassAlias({
+        '@sass': path.join(__dirname, '../../../assets', 'sass'),
+        '@fontawesome': path.join(__dirname, '../../../../node_modules', 'font-awesome'),
+      }).getImporter(),
+      // temporary measure until new versions of bootstrap and font-awesome released
+      quietDeps: true,
+      silenceDeprecations: ['import', 'legacy-js-api'],
+    },
+  });
 };
 
 module.exports = sassPlugin;
